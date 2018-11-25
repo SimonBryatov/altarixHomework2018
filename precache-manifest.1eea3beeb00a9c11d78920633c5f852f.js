41d78da7d1a50084104eeb33bb1124a33ac4b8f7
SimonBryatov <bsr-007@mail.ru>
2018-11-25 11:14:21 +0400

    Updates

---
+++ precache-manifest.1eea3beeb00a9c11d78920633c5f852f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36838acac84b8ac01adb",
+    "url": "/ReactChat/static/js/runtime~main.36838aca.js"
+  },
+  {
+    "revision": "bbbb969daf1c22cffa49",
+    "url": "/ReactChat/static/js/main.bbbb969d.chunk.js"
+  },
+  {
+    "revision": "3aaca4f79b57f2dffe45",
+    "url": "/ReactChat/static/js/1.3aaca4f7.chunk.js"
+  },
+  {
+    "revision": "bbbb969daf1c22cffa49",
+    "url": "/ReactChat/static/css/main.c4792f4b.chunk.css"
+  },
+  {
+    "revision": "3aaca4f79b57f2dffe45",
+    "url": "/ReactChat/static/css/1.b3844bf8.chunk.css"
+  },
+  {
+    "revision": "b86c3a1a4a2e0a206af24b5f964ded1d",
+    "url": "/ReactChat/index.html"
+  }
+];
